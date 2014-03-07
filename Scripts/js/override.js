a29e8bdb4800453053cf3c561548d0a3ceaa0fc2
NickyH <nicky.hughes@tapestry.net>
2014-03-07 14:18:54 +1100

    added first form

--- Scripts/js/override.js
+++ Scripts/js/override.js
@@ -1,17 +1,40 @@
 //dom ready functions
 $(function(){
-
-    $('.share-an-idea').on('click', start_idea);
+  insert_about();
+  $('.datepicker').pickadate();
+  $('.share-an-idea').on('click', start_idea);
 
 });
 
+function insert_about() {
+  $('.right-group').empty();
+  $.get('about.html', function(data) {
+      $('.right-group').html(data);
+  });
+}
+
+function insert_start() {
+  $('.right-group').empty();
+  $.get('start.html', function(data) {
+      $('.right-group').html(data);
+  });
+}
+
+function insert_top_bar() {
+  $.get('top_bar.html', function(data) {
+      $('#insert-top').html(data);
+  });
+}
 
 function start_idea() {
-$('.panel-about').parent().slideUp( 500, function() {
-    $('.panel-start-idea').parent().slideUp();
-    $('.panel-about').addClass('hidden');
-    $('.panel-start-idea').parent().slideDown( 500, function() {
-        $('.panel-start-idea').removeClass('hidden');
-        });
-    });
+  $('#main-page').parent().slideUp( 600, function() {
+    $('#main-page').slideUp( 50 );
+    insert_start();
+    insert_top_bar();
+    $('.share-an-idea').addClass('hidden');
+  });
+
+  $('#main-page').parent().slideDown( 50, function() {
+    $('#main-page').slideDown( 600 );
+  });
 }
