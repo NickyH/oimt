6f09edf97fc5783ee99c38d21d161dc22051b998
NickyH <nicky.hughes@tapestry.net>
2014-03-10 12:00:13 +1100

    header bar, completion form, bottom bar, logos and top right icons

--- Scripts/js/override.js
+++ Scripts/js/override.js
@@ -1,10 +1,17 @@
 //dom ready functions
 $(function(){
+  insert_header();
   insert_about();
   $('.datepicker').pickadate();
   $('.share-an-idea').on('click', start_idea);
   $('.submit-idea').on('click', insert_idea_to_project);
 });
+
+function insert_header() {
+  $.get('header_bar.html', function(data) {
+      $('#insert-header').html(data);
+  });
+}
 
 function insert_about() {
   $('.left-group').empty();
@@ -58,11 +65,33 @@ function insert_execution_phase() {
   $('#top-bar-idea-to-project').addClass('inactive');
   $('#top-bar-project-setup').addClass('inactive');
   $('#top-bar-execution').removeClass('inactive');
+  $('#top-bar-closeout').addClass('inactive');
+}
+
+function insert_closeout() {
+  console.log('closeout');
+  $('.left-group').empty();
+  $.get('closeout.html', function(data) {
+      $('.left-group').html(data);
+  });
+  insert_summary();
+
+  $('#top-bar-start').addClass('inactive');
+  $('#top-bar-idea-to-project').addClass('inactive');
+  $('#top-bar-project-setup').addClass('inactive');
+  $('#top-bar-execution').addClass('inactive');
+  $('#top-bar-closeout').removeClass('inactive');
 }
 
 function insert_top_bar() {
   $.get('top_bar.html', function(data) {
       $('#insert-top').html(data);
+  });
+}
+
+function insert_bottom_bar() {
+  $.get('bottom_bar.html', function(data) {
+      $('#insert-bottom').html(data);
   });
 }
 
@@ -78,6 +107,7 @@ function start_idea() {
     $('#main-page').slideUp( 50 );
     insert_start();
     insert_top_bar();
+    insert_bottom_bar();
     $('.share-an-idea').addClass('hidden');
     $('.submit-idea').removeClass('hidden');
   });
@@ -85,4 +115,12 @@ function start_idea() {
   $('#main-page').parent().slideDown( 50, function() {
     $('#main-page').slideDown( 600 );
   });
+}
+
+function nav_button_hover() {
+  $(this).addClass('nav-hover');
+}
+
+function nav_button_leave() {
+  $(this).removeClass('nav-hover');
 }
