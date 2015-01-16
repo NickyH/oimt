58ddaa92065c9e43fe5981520afe7937e51b876f
NickyH <nicky.hughes@tapestry.net>
2015-01-16 16:29:41 +1100

    added all expenditure tables

--- Scripts/js/tables.js
+++ Scripts/js/tables.js
@@ -69,11 +69,11 @@ $('#table-rd-exp-employees').dataTable({
       "defaultContent": '<select class="form-control" data-required="true"><option>Treatment 1</option><option>Treatment 2</option></select>'
     },
     {
-      "defaultContent": '<input type="text" class="form-control">'
+      "defaultContent": '<input type="text" class="form-control">',
+      "orderable": false
     },
     {
-      "defaultContent": '<input type="text" class="form-control data-required="true">',
-      "orderable": false
+      "defaultContent": '<input type="text" class="form-control data-required="true">'
     },
     {
       "defaultContent": '<input type="text" class="form-control">'
@@ -104,11 +104,155 @@ $('#table-rd-exp-contractors').dataTable({
       "defaultContent": '<select class="form-control" data-required="true"><option>Treatment 1</option><option>Treatment 2</option></select>'
     },
     {
+      "defaultContent": '<input type="text" class="form-control">',
+      "orderable": false
+    },
+    {
+      "defaultContent": '<input type="text" class="form-control data-required="true">'
+    },
+    {
       "defaultContent": '<input type="text" class="form-control">'
     },
     {
-      "defaultContent": '<input type="text" class="form-control data-required="true">',
+      "defaultContent": '<div class="text"><a href="#" title="Delete Row"><div class="glyphicon glyphicon-remove"></div></a></div>',
       "orderable": false
+    }
+  ]
+});
+
+//other direct costs
+$('#table-rd-exp-other').dataTable({
+  "bLengthChange": false,
+  "bInfo" : false,
+  "bPaginate" : false,
+  "columns": [
+    {
+      "defaultContent": '<input type="text" class="form-control" data-required="true">'
+    },
+    {
+      "defaultContent": '',
+      "orderable": false
+    },
+    {
+      "defaultContent": '<select class="form-control" data-required="true"><option>Method 1</option><option>Method 2</option></select>'
+    },
+    {
+      "defaultContent": '<select class="form-control" data-required="true"><option>Treatment 1</option><option>Treatment 2</option></select>'
+    },
+    {
+      "defaultContent": '<input type="text" class="form-control">',
+      "orderable": false
+    },
+    {
+      "defaultContent": '<input type="text" class="form-control data-required="true">'
+    },
+    {
+      "defaultContent": '<input type="text" class="form-control">'
+    },
+    {
+      "defaultContent": '<div class="text"><a href="#" title="Delete Row"><div class="glyphicon glyphicon-remove"></div></a></div>',
+      "orderable": false
+    }
+  ]
+});
+
+//overhead
+$('#table-rd-exp-overhead').dataTable({
+  "bLengthChange": false,
+  "bInfo" : false,
+  "bPaginate" : false,
+  "columns": [
+    {
+      "defaultContent": '<input type="text" class="form-control" data-required="true">'
+    },
+    {
+      "defaultContent": '',
+      "orderable": false
+    },
+    {
+      "defaultContent": '<select class="form-control" data-required="true"><option>Method 1</option><option>Method 2</option></select>'
+    },
+    {
+      "defaultContent": '<select class="form-control" data-required="true"><option>Treatment 1</option><option>Treatment 2</option></select>'
+    },
+    {
+      "defaultContent": '<input type="text" class="form-control">',
+      "orderable": false
+    },
+    {
+      "defaultContent": '<input type="text" class="form-control data-required="true">'
+    },
+    {
+      "defaultContent": '<input type="text" class="form-control">'
+    },
+    {
+      "defaultContent": '<div class="text"><a href="#" title="Delete Row"><div class="glyphicon glyphicon-remove"></div></a></div>',
+      "orderable": false
+    }
+  ]
+});
+
+//fixed assets
+$('#table-rd-exp-fixedassets').dataTable({
+  "bLengthChange": false,
+  "bInfo" : false,
+  "bPaginate" : false,
+  "columns": [
+    {
+      "defaultContent": '<input type="text" class="form-control" data-required="true">'
+    },
+    {
+      "defaultContent": '',
+      "orderable": false
+    },
+    {
+      "defaultContent": '<select class="form-control" data-required="true"><option>Method 1</option><option>Method 2</option></select>'
+    },
+    {
+      "defaultContent": '<select class="form-control" data-required="true"><option>Treatment 1</option><option>Treatment 2</option></select>'
+    },
+    {
+      "defaultContent": '<input type="text" class="form-control">',
+      "orderable": false
+    },
+    {
+      "defaultContent": '<input type="text" class="form-control data-required="true">'
+    },
+    {
+      "defaultContent": '<input type="text" class="form-control">'
+    },
+    {
+      "defaultContent": '<div class="text"><a href="#" title="Delete Row"><div class="glyphicon glyphicon-remove"></div></a></div>',
+      "orderable": false
+    }
+  ]
+});
+
+//feedstock expenditure
+$('#table-rd-exp-feedstock').dataTable({
+  "bLengthChange": false,
+  "bInfo" : false,
+  "bPaginate" : false,
+  "columns": [
+    {
+      "defaultContent": '<input type="text" class="form-control" data-required="true">'
+    },
+    {
+      "defaultContent": '',
+      "orderable": false
+    },
+    {
+      "defaultContent": '<select class="form-control" data-required="true"><option>Method 1</option><option>Method 2</option></select>'
+    },
+    {
+      "defaultContent": '<select class="form-control" data-required="true"><option>Treatment 1</option><option>Treatment 2</option></select>'
+    },
+    {
+      "defaultContent": '<input type="text" class="form-control">',
+      "orderable": false
+    },
+    {
+      "defaultContent": '<input type="text" class="form-control data-required="true">'
     },
     {
       "defaultContent": '<input type="text" class="form-control">'
